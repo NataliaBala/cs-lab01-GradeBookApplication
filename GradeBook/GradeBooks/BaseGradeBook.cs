a7ae8bc18ae2f5de65de36b98319ef0bf396cda7
Natalia Bała <natalia.bala@microsoft.wsei.edu.pl>
2024-03-21 14:14:18 +0100

    Task-17

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -19,7 +19,7 @@ namespace GradeBook.GradeBooks
 
         public List<Student> Students { get; set; }
 
-        //public bool IsWeighted { get; set; }
+        public bool IsWeighted { get; set; }
 
         public BaseGradeBook(string name)
         {
